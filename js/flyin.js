b75f487b866d1f19cda8ddc9a895f8563b32cfc9
Connor Sears <connor@twitter.com>
2012-08-20 22:56:21 -0700

    Derping on the transform origin shizz

--- js/flyin.js
+++ js/flyin.js
@@ -1,70 +1,76 @@
 (function($) {
-
- $.fn.flyin = function(options) {
-
-   var defaults = {
+  $.fn.flyin = function(options) {
+    // TODO: Determine all the defaults for flyin parameters
+    var defaults = {
      direction : 'false',
-     spin : 'false'
-   }
+     spin : 'false',
+     fade : 'false'
+    }
+    var options = $.extend({}, defaults, options);
+    return this.each(function() {
 
-   var options = $.extend({}, defaults, options);
-   return this.each(function() {
+      var flyingobject = $(this)
+      var flyingobjectHeight = flyingobject.height();
+      var flyingobjectWidth = flyingobject.width();
+      var flyingobjectOffset = flyingobject.offset();
 
-     var flyingobject = $(this)
-     var flyingobjectHeight = flyingobject.height();
-     var flyingobjectOffset = flyingobject.offset();
+      $(window).scroll(function() {
 
-     $(window).scroll(function() {
+        var scroll = window.pageYOffset;
+        var flyingobjectPosition = -($(window).height()/2) + (flyingobjectOffset.top + flyingobjectHeight/2) - scroll;
+        var flyingobjectRotation = 360-(scroll/(-($(window).height()/2) + (flyingobjectOffset.top + flyingobjectHeight/2))*360);
 
-       var scroll = window.pageYOffset;
-       var flyingobjectPosition = -($(window).height()/2) + (flyingobjectOffset.top + flyingobjectHeight/2) - scroll;
-       var flyingobjectRotation = 360-(scroll/(-($(window).height()/2) + (flyingobjectOffset.top + flyingobjectHeight/2))*360);
-       var flyingobjectPlacement;
-       var flyingobjectSpin;
+        // Depending on flyingobjectPosition and flyingobjectRotataion, determine where to place or how to spin
+        var flyingobjectPlacement;
+        var flyingobjectSpin;
 
-       if(flyingobjectPosition<=0){
+        // If the user is scrolled to the object placement on the screen, flyin should be finished.
+        if(flyingobjectPosition<=0){
          flyingobjectPlacement=0;
          flyingobjectSpin=0;
-       }
-       else{
+        }
+        // Or keep on flyin
+        else{
          flyingobjectPlacement=flyingobjectPosition;
          flyingobjectSpin=flyingobjectRotation;
-       }
-       //Handle the placment of the objects on the screen
-       if(options.direction=='false'){
-         flyingobject.css({
-       });
-       } else if(options.direction=='left'){
+        }
+
+        //Handle the placment of the objects on the screen
+        if(options.direction=='false'){
+          // do nothing
+        } else if(options.direction=='left'){
          flyingobject.css({
            "left" : flyingobjectPlacement+"px"
          });
-       } else if(options.direction=='right'){
+        } else if(options.direction=='right'){
          flyingobject.css({
            "right" : flyingobjectPlacement+"px"
          });
-       }
+        }
 
-       //Handle the spin of the objects on the screen
-       if(options.spin=='false'){
+        //Handle the spin of the objects on the screen
+        if(options.spin=='false'){
          flyingobject.css({
            "-webkit-transform" : "none",
            "-moz-transform" : "none",
            "transform" : "none"
-       });
-       } else if(options.spin=='left'){
+        });
+        } else if(options.spin=='left'){
          flyingobject.css({
            "-webkit-transform" : "rotate("+flyingobjectSpin+"deg)",
            "-moz-transform" : "rotate("+flyingobjectSpin+"deg)",
-           "transform" : "rotate("+flyingobjectSpin+"deg)"
+           "transform" : "rotate("+flyingobjectSpin+"deg)",
+           "-webkit-transform-origin" : "0% "+flyingobjectWidth*4+"px"
          });
-       } else if(options.spin=='right'){
+        } else if(options.spin=='right'){
          flyingobject.css({
            "-webkit-transform" : "rotate(-"+flyingobjectSpin+"deg)",
            "-moz-transform" : "rotate(-"+flyingobjectSpin+"deg)",
-           "transform" : "rotate(-"+flyingobjectSpin+"deg)"
+           "transform" : "rotate(-"+flyingobjectSpin+"deg)",
+           "-webkit-transform-origin" : flyingobjectWidth*4+"px "+flyingobjectWidth*4+"px"
          });
-       }
-     });
-   });
- }
+        }
+      });
+    });
+  }
 })(jQuery);
